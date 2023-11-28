5ecce0ffa54e0be0a60e4e325c61b85584674fde
Stella-Remember <s.habiyambe@alustudent.com>
2023-11-28 14:00:44 -0800

    Network_1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
+"""Fetches https://alu-intranet.hbtn.io/status"""
 
+from urllib import request
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+url = "https://alu-intranet.hbtn.io/status"
+
+with request.urlopen(url) as response:
+    content = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
